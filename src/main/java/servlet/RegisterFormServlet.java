354040c68e007a0cb036bd295ecf7c337fba8305
CherryFunCode <CherryFunCode>
2022-08-10 20:06:08 +0930

    Merge branch 'main' of https://github.com/Trainee-Project/7.git into main

--- src/main/java/servlet/RegisterFormServlet.java
+++ src/main/java/servlet/RegisterFormServlet.java
@@ -7,6 +7,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
